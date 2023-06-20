d046400ebb15eb8d4465575c6f6a7a9b4638ac8e
hotamago <nhson2003@outlook.com>
2023-06-21 01:13:46 +0700

    Update gptCommands.py

--- constants/gptCommands.py
+++ constants/gptCommands.py
@@ -4,6 +4,7 @@ def gptCommands_animegirlvoice(text):
 - only return japanese
 - translate it into Japanese with a cute voice like the cat girls in the anime (Always end with "nya")
 - no transcription added
+- if it is a noun or can not be translated into Japanese, change it to the same pronunciation as in Japanese
 - when you finish add @finish to the end of result""".format(text=text)
 
     return promt
