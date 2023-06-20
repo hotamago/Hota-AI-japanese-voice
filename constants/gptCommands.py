5732380dfb0cbcd72c7d79feb253a37676cc309f
hotamago <nhson2003@outlook.com>
2023-06-21 01:24:11 +0700

    Update gptCommands.py

--- constants/gptCommands.py
+++ constants/gptCommands.py
@@ -2,9 +2,10 @@ def gptCommands_animegirlvoice(text):
     promt = """translate text "{text}" from vietnamese to japanese the result much follow the rule below:
 - no explanation
 - only return japanese
+- NO LATIN CHARACTERS
 - translate it into Japanese with a cute voice like the cat girls in the anime (Always end with "nya")
 - no transcription added
-- if it is a noun or can not be translated into Japanese, change it to the same pronunciation as in Japanese
+- noun or word can not be translated, should change all it to the same pronunciation as in Japanese word
 - when you finish add @finish to the end of result""".format(text=text)
 
     return promt
